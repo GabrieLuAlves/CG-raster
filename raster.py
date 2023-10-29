0d9b691674fa7fa95fad62d0e88165bb4afb2193
Gabriel Luan <gabrielluan.valentim@gmail.com>
2023-10-28 21:22:06 -0300

    feature: added the possibility to choose the number of points in a hermite curve

--- raster.py
+++ raster.py
@@ -4,8 +4,6 @@ from os import path, mkdir
 import PIL.Image
 from PIL.Image import Image
 
-import matplotlib.pyplot as plt
-
 from typing import List, Tuple
 
 
@@ -101,7 +99,7 @@ class Field:
 
         self._lines: List[Line] = []
         self._polygons: List[Polygon] = []
-        self._curves: List[HermineCurve] = []
+        self._curves: List[Tuple[HermineCurve, int]] = []
     
     @property
     def resolution(self):
@@ -117,8 +115,8 @@ class Field:
     def add_polygon(self, polygon: Polygon):
         self._polygons.append(polygon)
     
-    def add_hermine_curve(self, curve: HermineCurve):
-        self._curves.append(curve)
+    def add_hermine_curve(self, curve: HermineCurve, n_points: int):
+        self._curves.append((curve, n_points))
     
     def _map_points(self, points: List[Point]) -> List[Point]:
         width, height = self._resolution
@@ -286,8 +284,8 @@ class Field:
         field = np.zeros((rows, columns))
         points: List[np.ndarray] = []
 
-        for curve in self._curves:
-            points.append(self._raster_hermine_curve(curve, 10))
+        for curve, n_points in self._curves:
+            points.append(self._raster_hermine_curve(curve, n_points))
 
         for polygon in self._polygons:
             points.append(self._draw_polygon(polygon))
@@ -427,25 +425,25 @@ def main():
                 (+radius, +0.0),
                 (+1.675 * radius, +0.0),
                 (+0.0, -1.675 * radius),
-            ))
+            ), 20)
             field.add_hermine_curve((
                 (+radius, +0.0),
                 (+0.0, -radius),
                 (+0.0, -1.675 * radius),
                 (-1.675 * radius, +0.0),
-            ))
+            ), 20)
             field.add_hermine_curve((
                 (+0.0, -radius),
                 (-radius, +0.0),
                 (-1.675 * radius, +0.0),
                 (+0.0, +1.675 * radius),
-            ))
+            ), 20)
             field.add_hermine_curve((
                 (-radius, +0.0),
                 (+0.0, +radius),
                 (+0.0, +1.675 * radius),
                 (+1.675 * radius, +0.0),
-            ))
+            ), 20)
         
         random_numbers = np.random.random((16, 2))
         random_numbers[:, 0] = 0.75 * random_numbers[:, 0] + 0.1
@@ -461,7 +459,7 @@ def main():
             x, y = random_numbers[i]
             i += 1
 
-            field.add_hermine_curve(((x, y), (x, y), (-0.15, +0.2), (-0.15, -0.2)))
+            field.add_hermine_curve(((x, y), (x, y), (-0.15, +0.2), (-0.15, -0.2)), 10)
 
         for _ in range(4):
             x, y = random_numbers[i]
